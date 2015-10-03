4323a98314d9ac126804c1743ad4c70c4cca5475
Mick <i_man90@mail.ru>
2015-10-04 01:47:41 +0500

    Revert "Add files"
    This reverts commit 8a8177d8b7eb8983eece8523d62148e19d68a8f8.

--- SyncWorker/PacketTypes.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "MessageIdentifiers.h"
-
-enum Packets{
-	CREATE_WORLD = ID_USER_PACKET_ENUM + 1,
-	UPDATE_ENTITY = CREATE_WORLD + 1,
-	ADD_ENTITY = UPDATE_ENTITY + 1,
-	REMOVE_ENTITY = ADD_ENTITY + 1
-};
